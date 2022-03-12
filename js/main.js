2206cb1216902d75e98d616ff8158ffcc8dcc41b
Aftab Khalid <iffi.d.s@gmail.com>
2022-03-13 02:44:11 +0500

    added form validation

--- js/main.js
+++ js/main.js
@@ -142,26 +142,6 @@ MM_preloadImages(
 //   $('#gform').prepend('Your submission has been processed...');
 // });
 
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
 //
 // Particals for canvas
 
