a0abbf46ffc06f4f87b8ea2cf0e4deb425caf0d5
Aftab Khalid <iffi.d.s@gmail.com>
2022-03-13 03:22:58 +0500

    added form validation

--- js/main.js
+++ js/main.js
@@ -146,10 +146,10 @@ MM_preloadImages(
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var myForms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
+  Array.prototype.slice.call(myForms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
