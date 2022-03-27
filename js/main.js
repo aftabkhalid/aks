53b17f464bb5c75e5d9435a8927acd6f3cd53968
Aftab Khalid <iffi.d.s@gmail.com>
2022-03-27 22:56:54 +0500

    random backgrounds in header

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ document.body.onload = function() {
     '../images/header-bg-5.jpg',
     '../images/header-bg-6.jpg',
     '../images/header-bg-7.jpg',
-    '../images/header-bg-8.jpg',
+    '../images/header-bg-8.jpg'
   );
   var numBg = backgrounds.length;
   if (document.images) {
